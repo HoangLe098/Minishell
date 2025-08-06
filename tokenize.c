41ed08a112510e70386d88a43a987af02a1dd4bb
Hoang Le <le.th.hoang98@gmail.com>
2025-08-07 00:02:30 +0300

    debug tokenize

--- tokenize.c
+++ tokenize.c
@@ -1,27 +1,8 @@
 #include "minishell.h"
 
-#include "minishell.h"
-
 static int	is_operator_char(char c)
 {
 	return (c == '<' || c == '>' || c == '|');
-}
-
-static int	word_has_dollar(const char *s, int i)
-{
-	while (s[i] && s[i] != ' ')
-	{
-		if (s[i] == '$')
-			return (1);
-		if ((s[i] == '\'' || s[i] == '\"'))
-		{
-			char quote = s[i++];
-			while (s[i] && s[i] != quote)
-				i++;
-		}
-		i++;
-	}
-	return (0);
 }
 
 static int	add_token(t_token **head, const char *start, int len, int is_op)
@@ -49,42 +30,44 @@ static int	add_token(t_token **head, const char *start, int len, int is_op)
 	return (1);
 }
 
-static int	skip_quotes(const char *s, int *i, char *buf, int *j)
-{
-	char	quote;
-
-	quote = s[*i];
-	(*i)++;
-	while (s[*i] && s[*i] != quote)
-		buf[(*j)++] = s[(*i)++];
-	if (s[*i] == quote)
-		(*i)++;
-	return (0);
-}
-
 static void	process_word(const char *s, int *i, t_token **head)
 {
 	char	buf[1024];
 	int		j;
+	char	quote;
 
 	j = 0;
-	if (word_has_dollar(s, *i))
-	{
-		// Copy everything until next space into buf
-		while (s[*i] && s[*i] != ' ')
-			buf[j++] = s[(*i)++];
-		add_token(head, buf, j, 0);
-		return ;
-	}
 	while (s[*i] && s[*i] != ' ')
 	{
 		if (s[*i] == '\'' || s[*i] == '\"')
-			skip_quotes(s, i, buf, &j);
+		{
+			quote = s[*i];
+			(*i)++;
+			while (s[*i] && s[*i] != quote)
+				buf[j++] = s[(*i)++];
+			if (s[*i] == quote)
+				(*i)++;
+		}
+		else if (s[*i] == '$')
+		{
+			if (j > 0)
+			{
+				add_token(head, buf, j, 0);
+				j = 0;
+			}
+			buf[j++] = s[(*i)++];
+			while (s[*i] && (ft_isalnum(s[*i]) || s[*i] == '_'))
+				buf[j++] = s[(*i)++];
+			add_token(head, buf, j, 0);
+			j = 0;
+		}
 		else if (is_operator_char(s[*i]))
 		{
 			if (j > 0)
+			{
 				add_token(head, buf, j, 0);
-			j = 0;
+				j = 0;
+			}
 			if ((s[*i] == '<' || s[*i] == '>') && s[*i + 1] == s[*i])
 				add_token(head, &s[*i], 2, 1), (*i) += 2;
 			else
